fe030cb9b7c44fe024255e91b2a78277a4c9f29d
Samuel George <samuegeorge58@gmail.com>
2019-10-18 06:43:31 +0100

    fix image related issues

--- app/Blog.php
+++ app/Blog.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content', 'image_path',
+    ];
+
 }
