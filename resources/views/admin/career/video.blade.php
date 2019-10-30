88b2847211456c64a69f7c4083a81fe150f1fcda
Samuel George <samuegeorge58@gmail.com>
2019-10-31 00:44:43 +0100

    updates based on feedback

--- resources/views/admin/career/video.blade.php
+++ resources/views/admin/career/video.blade.php
@@ -3,47 +3,51 @@
 @section('title','VFD Group Career Video')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
+<div class="content-page">
+    <div class="content">
+        <div class="container">
 
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card-box">
-                            <h4 class="header-title m-t-0 m-b-30">Upload Video link</h4>
-                            
-                            <div class="row">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-box">
+                        <h4 class="header-title m-t-0 m-b-30">Upload Video link</h4>
+
+                        <div class="row">
                             {{-- @include('partial.alert') --}}
-                                    <form class="form-horizontal" action="{{ url('admin/career/video') }}" method="post" enctype="multipart/form-data" role="form">
-                                        {{ csrf_field() }}
-                                        <div class="col-md-10">
-                                            <div class="form-group">
-                                                <label class="control-label col-md-2 col-sm-3">
-                                                    Youtube Link
-                                                </label>
-                                                <div class="col-md-9 col-sm-9">
-                                                <input class="form-control" name="link" value="{{$company->youtube_link}}" />
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="col-md-11">
-                                                    <button class="btn btn-lg btn-primary pull-right">Submit</button>
-                                                </div>
-                                            </div>
-                                          
-                                        </form>
-                                </div>
-                                @if($company->youtube_link)
-                                <div class="col-md-offset-1 col-md-6">
-                                    <div class="video-container">
-                                        <iframe width="915" height="515" src={{$company->youtube_link}} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <form class="form-horizontal" action="{{ url('admin/career/video') }}" method="post"
+                                enctype="multipart/form-data" role="form">
+                                {{ csrf_field() }}
+                                <div class="col-md-10">
+                                    <div class="form-group">
+                                        <label class="control-label col-md-2 col-sm-3">
+                                            Youtube Link
+                                        </label>
+                                        <div class="col-md-9 col-sm-9">
+                                            <input class="form-control" name="link"
+                                                value="{{$company->youtube_link}}" />
+                                        </div>
                                     </div>
-                                </div>
-                                @endif
+                                    <div class="form-group">
+                                        <div class="col-md-11">
+                                            <button class="btn btn-lg btn-primary pull-right">Submit</button>
+                                        </div>
+                                    </div>
+
+                            </form>
+                        </div>
+                        @if($company->youtube_link)
+                        <div class="col-md-offset-1 col-md-6">
+                            <div class="video-container">
+                                <iframe width="915" height="515" src={{$company->youtube_link}} frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
                             </div>
                         </div>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
