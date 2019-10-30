aecb7518e7b3ee4db986b23ca8cf8a8233431b1b
Samuel George <samuegeorge58@gmail.com>
2019-10-31 00:57:27 +0100

    updates based on feedback

--- resources/views/admin/governance/index.blade.php
+++ resources/views/admin/governance/index.blade.php
@@ -3,52 +3,51 @@
 @section('title','Governance')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class=" m-t-0 m-b-30">Board of Directors</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance/directors') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class=" m-t-0 m-b-30">Board of Directors</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance/directors') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Head of Portfolios</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance/portfolios') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Management Team</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a
-                                                href="{{ url('admin/governance/management') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Head of Portfolios</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance/portfolios') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Management Team</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance/management') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
+</div>
 @endsection
