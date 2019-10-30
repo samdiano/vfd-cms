899d573b270284d4f9e32c1541ef19ce8043d848
Samuel George <samuegeorge58@gmail.com>
2019-10-31 00:58:33 +0100

    updates based on feedback

--- resources/views/admin/pages/about.blade.php
+++ resources/views/admin/pages/about.blade.php
@@ -3,79 +3,77 @@
 @section('title','About Page')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class=" m-t-0 m-b-30">About us</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/about') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class=" m-t-0 m-b-30">About us</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/about') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Governance</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Governance</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/governance') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Our History</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a
-                                                href="{{ url('admin/history') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Our History</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/history') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Our Impact</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a
-                                                href="{{ url('admin/impact') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Our Impact</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/impact') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    {{-- <div class="col-lg-3 col-md-6">
+                {{-- <div class="col-lg-3 col-md-6">
                         <div class="card-box text-center">
                             <h2 class="m-t-0 m-b-30">Our Values</h2>
                             <div class="widget-chart-3">
                                 <div class="widget-detail-1 ">
                                     <h4 class="p-t-10 m-b-0"><a
                                                 href="{{ url('admin/values') }}">View</a></h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
-                            </div>
-                        </div>
-                    </div> --}}
-
-                </div>
+                {{-- <p class="text-muted">Registered</p> --}}
             </div>
         </div>
     </div>
+</div> --}}
+
+</div>
+</div>
+</div>
+</div>
 @endsection
