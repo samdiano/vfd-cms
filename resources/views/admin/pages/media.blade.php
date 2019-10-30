6882c97b7229af7d5fafb8455f4655cac3d43b4f
Samuel George <samuegeorge58@gmail.com>
2019-10-31 00:59:05 +0100

    updates based on feedback

--- resources/views/admin/pages/media.blade.php
+++ resources/views/admin/pages/media.blade.php
@@ -3,38 +3,38 @@
 @section('title','Media Page')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class=" m-t-0 m-b-30">Blog</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/blog') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class=" m-t-0 m-b-30">Blog</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/blog') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Gallery</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/gallery') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Gallery</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/gallery') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
+</div>
 @endsection
