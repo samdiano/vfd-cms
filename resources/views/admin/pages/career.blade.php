adc739d2d0cb6e89394d0be25ebb5803c0dc85eb
Samuel George <samuegeorge58@gmail.com>
2019-10-31 00:58:43 +0100

    updates based on feedback

--- resources/views/admin/pages/career.blade.php
+++ resources/views/admin/pages/career.blade.php
@@ -3,79 +3,77 @@
 @section('title','Career Page')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class=" m-t-0 m-b-30">Career</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/career') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class=" m-t-0 m-b-30">Career</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/career') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Vacancies</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/vacancies') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Vacancies</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/vacancies') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card-box text-center">
-                            <h2 class="m-t-0 m-b-30">Portfolio Vacancies</h2>
-                            <div class="widget-chart-3">
-                                <div class="widget-detail-1 ">
-                                    <h4 class="p-t-10 m-b-0"><a
-                                                href="{{ url('admin/portfolio-vacancies') }}">View</a>
-                                    </h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Portfolio Vacancies</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/portfolio-vacancies') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-lg-3 col-md-6">
-                            <div class="card-box text-center">
-                                <h2 class="m-t-0 m-b-30">Youtube Video</h2>
-                                <div class="widget-chart-3">
-                                    <div class="widget-detail-1 ">
-                                        <h4 class="p-t-10 m-b-0"><a
-                                                    href="{{ url('admin/career/video') }}">View</a>
-                                        </h4>
-                                        {{-- <p class="text-muted">Registered</p> --}}
-                                    </div>
-                                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-box text-center">
+                        <h2 class="m-t-0 m-b-30">Youtube Video</h2>
+                        <div class="widget-chart-3">
+                            <div class="widget-detail-1 ">
+                                <h4 class="p-t-10 m-b-0"><a href="{{ url('admin/career/video') }}">View</a>
+                                </h4>
+                                {{-- <p class="text-muted">Registered</p> --}}
                             </div>
                         </div>
-    
-                    {{-- <div class="col-lg-3 col-md-6">
+                    </div>
+                </div>
+
+                {{-- <div class="col-lg-3 col-md-6">
                         <div class="card-box text-center">
                             <h2 class="m-t-0 m-b-30">Our Values</h2>
                             <div class="widget-chart-3">
                                 <div class="widget-detail-1 ">
                                     <h4 class="p-t-10 m-b-0"><a
                                                 href="{{ url('admin/values') }}">View</a></h4>
-                                    {{-- <p class="text-muted">Registered</p> --}}
-                                </div>
-                            </div>
-                        </div>
-                    </div> --}}
-
-                </div>
+                {{-- <p class="text-muted">Registered</p> --}}
             </div>
         </div>
     </div>
+</div> --}}
+
+</div>
+</div>
+</div>
+</div>
 @endsection
