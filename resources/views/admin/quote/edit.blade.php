b2cc85d30c115f533c17e5e337b711c5fc3d429a
Samuel George <samuegeorge58@gmail.com>
2019-10-31 01:00:59 +0100

    updates based on feedback

--- resources/views/admin/quote/edit.blade.php
+++ resources/views/admin/quote/edit.blade.php
@@ -3,42 +3,44 @@
 @section('title','Quote')
 
 @section('content')
-    <div class="content-page">
-      <script>
+<div class="content-page">
+    <script>
         // tinymce.activeEditor.dom.addClass(tinymce.activeEditor.dom.select('p'), 'myclass');
-      </script>
-        <div class="content">
-            <div class="container">
+    </script>
+    <div class="content">
+        <div class="container">
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card-box">
-                            <h4 class="header-title m-t-0 m-b-30">Quote</h4>
-                            <div class="row">
-                                    {{-- @include('partial.alert') --}}
-                                    <form class="form-horizontal" action="{{ url('admin/quote') }}" method="post" enctype="multipart/form-data" role="form">
-                                        {{ csrf_field() }}
-                                       
-                                            <div class="form-group">
-                                                <label class="control-label col-md-2 col-sm-3">
-                                                    Text
-                                                </label>
-                                                <div class="col-md-8 col-sm-9">
-                                                <input type="text" class="form-control" style="height:100px" name="quote" value="{{$company->quote}}" />
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="col-md-10">
-                                                    <button class="btn btn-lg btn-primary pull-right">Submit</button>
-                                                </div>
-                                            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-box">
+                        <h4 class="header-title m-t-0 m-b-30">Quote</h4>
+                        <div class="row">
+                            {{-- @include('partial.alert') --}}
+                            <form class="form-horizontal" action="{{ url('admin/quote') }}" method="post"
+                                enctype="multipart/form-data" role="form">
+                                {{ csrf_field() }}
 
-                                        </form>
+                                <div class="form-group">
+                                    <label class="control-label col-md-2 col-sm-3">
+                                        Text
+                                    </label>
+                                    <div class="col-md-8 col-sm-9">
+                                        <input type="text" class="form-control" style="height:100px" name="quote"
+                                            value="{{$company->quote}}" />
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="form-group">
+                                    <div class="col-md-10">
+                                        <button class="btn btn-lg btn-primary pull-right">Submit</button>
+                                    </div>
+                                </div>
+
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
