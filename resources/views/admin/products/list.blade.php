1b6ed78a28c57594f0dd5ad7de0f923ae7948d63
Samuel George <samuegeorge58@gmail.com>
2019-10-31 01:00:28 +0100

    updates based on feedback

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -3,72 +3,81 @@
 @section('title','Products')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card-box">
-                             {{-- <a class="btn btn-primary" href="{{ url('admin/portfolio/services/new/') }}">Add New Service</a> --}}
-                            <div class="table-rep-plugin">
-                                <div class="table-responsive" data-pattern="priority-columns">
-                                    {{-- @include('partial.alert') --}}
-                                    @if(count($services) > 1)
-                                        <br><br>
-                                        <div class="alert alert-info text-center">
-                                            <p>There are no services at the moment</p>
-                                        </div>
-                                    @else
-                                        <br><br>
-                                        <table id="tech-companies-1" class="table  table-striped">
-                                            <thead>
-                                            <tr>
-                                                <th>Products</th>
-                                                <th>Image</th>
-                                                <th>Action</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            @foreach($products as $product)
-                                                <tr>
-                                                    <td>{{ $product->name }}</td>
-        
-                                                    <td>
-                                                        <a class="btn btn-info" href="{{ url('booking/annual/view/') }}">Edit</a>
-                                                        <a data-toggle="modal" data-target="#mod-danger" href="#" class="btn btn-danger">Delete</a>
-                                                    </td>
-                                                </tr>
-                                                <div id="mod-danger" tabindex="-1" role="dialog" style="" class="modal fade">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <button type="button" data-dismiss="modal" aria-hidden="true" class="close"><span class="mdi mdi-close"></span></button>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-box">
+                        {{-- <a class="btn btn-primary" href="{{ url('admin/portfolio/services/new/') }}">Add New
+                        Service</a> --}}
+                        <div class="table-rep-plugin">
+                            <div class="table-responsive" data-pattern="priority-columns">
+                                {{-- @include('partial.alert') --}}
+                                @if(count($services) > 1)
+                                <br><br>
+                                <div class="alert alert-info text-center">
+                                    <p>There are no services at the moment</p>
+                                </div>
+                                @else
+                                <br><br>
+                                <table id="tech-companies-1" class="table  table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Products</th>
+                                            <th>Image</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($products as $product)
+                                        <tr>
+                                            <td>{{ $product->name }}</td>
+
+                                            <td>
+                                                <a class="btn btn-info"
+                                                    href="{{ url('booking/annual/view/') }}">Edit</a>
+                                                <a data-toggle="modal" data-target="#mod-danger" href="#"
+                                                    class="btn btn-danger">Delete</a>
+                                            </td>
+                                        </tr>
+                                        <div id="mod-danger" tabindex="-1" role="dialog" style="" class="modal fade">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <button type="button" data-dismiss="modal" aria-hidden="true"
+                                                            class="close"><span class="mdi mdi-close"></span></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="text-center">
+                                                            <div class="text-danger"><span
+                                                                    class="modal-main-icon mdi mdi-close-circle-o"></span>
                                                             </div>
-                                                            <div class="modal-body">
-                                                                <div class="text-center">
-                                                                    <div class="text-danger"><span class="modal-main-icon mdi mdi-close-circle-o"></span></div>
-                                                                    <h3>Warning!</h3>
-                                                                    <p>You are about to delete a request.</p>
-                                                                    <div class="xs-mt-50">
-                                                                        <button type="button" data-dismiss="modal" class="btn btn-space btn-default">Cancel</button>
-                                                                        <a href="{{ url('booking/annual/')}}" class="btn btn-space btn-danger" type="submit">Delete</a>
-                                                                    </div>
-                                                                </div>
+                                                            <h3>Warning!</h3>
+                                                            <p>You are about to delete a request.</p>
+                                                            <div class="xs-mt-50">
+                                                                <button type="button" data-dismiss="modal"
+                                                                    class="btn btn-space btn-default">Cancel</button>
+                                                                <a href="{{ url('booking/annual/')}}"
+                                                                    class="btn btn-space btn-danger"
+                                                                    type="submit">Delete</a>
                                                             </div>
-                                                            <div class="modal-footer"></div>
                                                         </div>
                                                     </div>
-                                                </div> 
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    @endif
-                                </div>
-                                {{-- {{ $bookings->links() }} --}}
+                                                    <div class="modal-footer"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                @endif
                             </div>
+                            {{-- {{ $bookings->links() }} --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-@endsection
+    </div>
+    @endsection
