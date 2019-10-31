0dce4994ddb765ccc5d3a46eed742dc8703c6bc4
Samuel George <samuegeorge58@gmail.com>
2019-10-31 01:02:24 +0100

    updates based on feedback

--- resources/views/admin/vacancies/dynasty-vacancies.blade.php
+++ resources/views/admin/vacancies/dynasty-vacancies.blade.php
@@ -3,79 +3,90 @@
 @section('title','Dynasty Real Estate Vacancies')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card-box">
-                             <a class="btn btn-primary" href="{{ url('admin/portfolio/vacancies/new/') }}">Add New Portfolio Vacancy</a>
-                            <div class="table-rep-plugin">
-                                <div class="table-responsive" data-pattern="priority-columns">
-                                    {{-- @include('partial.alert') --}}
-                                    @if(count($vacancies) < 1)
-                                        <br><br>
-                                        <div class="alert alert-info text-center">
-                                            <p>There are no vacancies at the moment</p>
-                                        </div>
+<div class="content-page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-box">
+                        <a class="btn btn-primary" href="{{ url('admin/portfolio/vacancies/new/') }}">Add New Portfolio
+                            Vacancy</a>
+                        <div class="table-rep-plugin">
+                            <div class="table-responsive" data-pattern="priority-columns">
+                                {{-- @include('partial.alert') --}}
+                                @if(count($vacancies) < 1) <br><br>
+                                    <div class="alert alert-info text-center">
+                                        <p>There are no vacancies at the moment</p>
+                                    </div>
                                     @else
-                                        <br><br>
-                                        <table id="tech-companies-1" class="table  table-striped">
-                                            <thead>
+                                    <br><br>
+                                    <table id="tech-companies-1" class="table  table-striped">
+                                        <thead>
                                             <tr>
                                                 <th>Role</th>
                                                 <th>Description</th>
                                                 <th>Status</th>
                                                 <th>Action</th>
                                             </tr>
-                                            </thead>
-                                            <tbody>
+                                        </thead>
+                                        <tbody>
                                             @foreach($vacancies as $vacancy)
-                                                <tr>
-                                                    <td>{{ $vacancy->role }}</td>
-                                                    <td>{{ $vacancy->description }}</td>
-                                                    @if($vacancy->active == true)
-                                                        <td><button class="btn btn-success">Active</button></td>
-                                                    @else
-                                                        <td><button class="btn btn-warning">Not active</button></td>
-                                                    @endif
-                                                    <td>
-                                                        {{-- <a class="btn btn-primary" href="{{ url('booking/annual/view/') }}">View</a> --}}
-                                                        {{-- <a class="btn btn-info" href="{{ url('booking/annual/view/') }}">Edit</a> --}}
-                                                        <a data-toggle="modal" data-target="#mod-danger" href="#" class="btn btn-danger">Delete</a>
-                                                    </td>
-                                                </tr>
-                                                <div id="mod-danger" tabindex="-1" role="dialog" style="" class="modal fade">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <button type="button" data-dismiss="modal" aria-hidden="true" class="close"><span class="mdi mdi-close"></span></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <div class="text-center">
-                                                                    <div class="text-danger"><span class="modal-main-icon mdi mdi-close-circle-o"></span></div>
-                                                                    <h3>Warning!</h3>
-                                                                    <p>You are about to delete a request.</p>
-                                                                    <div class="xs-mt-50">
-                                                                        <button type="button" data-dismiss="modal" class="btn btn-space btn-default">Cancel</button>
-                                                                        <a href="{{ url('booking/annual/')}}" class="btn btn-space btn-danger" type="submit">Delete</a>
-                                                                    </div>
+                                            <tr>
+                                                <td>{{ $vacancy->role }}</td>
+                                                <td>{{ $vacancy->description }}</td>
+                                                @if($vacancy->active == true)
+                                                <td><button class="btn btn-success">Active</button></td>
+                                                @else
+                                                <td><button class="btn btn-warning">Not active</button></td>
+                                                @endif
+                                                <td>
+                                                    {{-- <a class="btn btn-primary" href="{{ url('booking/annual/view/') }}">View</a>
+                                                    --}}
+                                                    {{-- <a class="btn btn-info" href="{{ url('booking/annual/view/') }}">Edit</a>
+                                                    --}}
+                                                    <a data-toggle="modal" data-target="#mod-danger" href="#"
+                                                        class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                            <div id="mod-danger" tabindex="-1" role="dialog" style=""
+                                                class="modal fade">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" data-dismiss="modal"
+                                                                aria-hidden="true" class="close"><span
+                                                                    class="mdi mdi-close"></span></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <div class="text-center">
+                                                                <div class="text-danger"><span
+                                                                        class="modal-main-icon mdi mdi-close-circle-o"></span>
+                                                                </div>
+                                                                <h3>Warning!</h3>
+                                                                <p>You are about to delete a request.</p>
+                                                                <div class="xs-mt-50">
+                                                                    <button type="button" data-dismiss="modal"
+                                                                        class="btn btn-space btn-default">Cancel</button>
+                                                                    <a href="{{ url('booking/annual/')}}"
+                                                                        class="btn btn-space btn-danger"
+                                                                        type="submit">Delete</a>
                                                                 </div>
                                                             </div>
-                                                            <div class="modal-footer"></div>
                                                         </div>
+                                                        <div class="modal-footer"></div>
                                                     </div>
-                                                </div> 
+                                                </div>
+                                            </div>
                                             @endforeach
-                                            </tbody>
-                                        </table>
+                                        </tbody>
+                                    </table>
                                     @endif
-                                </div>
-                                {{-- {{ $bookings->links() }} --}}
                             </div>
+                            {{-- {{ $bookings->links() }} --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-@endsection
+    </div>
+    @endsection
