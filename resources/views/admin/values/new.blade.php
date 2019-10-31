ce1585bad9c0e89a534d94620b8cc76a16f9b04d
Samuel George <samuegeorge58@gmail.com>
2019-10-31 01:04:17 +0100

    updates based on feedback

--- resources/views/admin/values/new.blade.php
+++ resources/views/admin/values/new.blade.php
@@ -3,46 +3,49 @@
 @section('title','New Value')
 
 @section('content')
-    <div class="content-page">
-        <div class="content">
-            <div class="container">
+<div class="content-page">
+    <div class="content">
+        <div class="container">
 
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="card-box">
-                            <h4 class="header-title m-t-0 m-b-30">Create New Value</h4>
-                            <div class="row">
-{{-- @include('partial.alert') --}}
-                                    <form class="form-horizontal" action="{{ url('admin/values/new') }}" method="post" enctype="multipart/form-data" role="form">
-                                        {{ csrf_field() }}
-                                        <div class="col-lg-6">
-                                            <div class="form-group">
-                                                <label class="control-label col-md-3 col-sm-3">
-                                                    Name
-                                                </label>
-                                                <div class="col-md-9 col-sm-9">
-                                                    <input type="text" name="name" value="{{ Auth::User()->full_name }}" class="form-control">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                    <label class="control-label col-md-3 col-sm-3">
-                                                        Description
-                                                    </label>
-                                                    <div class="col-md-9 col-sm-9">
-                                                        <input type="text" name="description" value="{{ Auth::User()->full_name }}" class="form-control">
-                                                    </div>
-                                                </div>
-                                            <div class="form-group">
-                                                <div class="col-md-12">
-                                                    <button class="btn btn-lg btn-primary pull-right">Submit</button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                </div>
-                            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-box">
+                        <h4 class="header-title m-t-0 m-b-30">Create New Value</h4>
+                        <div class="row">
+                            {{-- @include('partial.alert') --}}
+                            <form class="form-horizontal" action="{{ url('admin/values/new') }}" method="post"
+                                enctype="multipart/form-data" role="form">
+                                {{ csrf_field() }}
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3">
+                                            Name
+                                        </label>
+                                        <div class="col-md-9 col-sm-9">
+                                            <input type="text" name="name" value="{{ Auth::User()->full_name }}"
+                                                class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3">
+                                            Description
+                                        </label>
+                                        <div class="col-md-9 col-sm-9">
+                                            <input type="text" name="description" value="{{ Auth::User()->full_name }}"
+                                                class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-md-12">
+                                            <button class="btn btn-lg btn-primary pull-right">Submit</button>
+                                        </div>
+                                    </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
