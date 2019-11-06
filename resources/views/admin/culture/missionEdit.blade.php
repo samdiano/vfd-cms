424c98e0b749be66a9af2aa5b0c2b26346d1b1e8
Samuel George <samuegeorge58@gmail.com>
2019-11-06 02:36:15 +0100

    updates based on feedback

--- resources/views/admin/culture/missionEdit.blade.php
+++ resources/views/admin/culture/missionEdit.blade.php
@@ -15,7 +15,7 @@
                     <div class="card-box">
                         <h4 class="header-title m-t-0 m-b-30">Mission</h4>
                         <div class="row">
-                            {{-- @include('partial.alert') --}}
+                            @include('partial.alert')
                             <form class="form-horizontal" action="{{ url('admin/mission') }}" method="post"
                                 enctype="multipart/form-data" role="form">
                                 {{ csrf_field() }}
